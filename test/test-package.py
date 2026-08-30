b44d57d9a93b49293d87708bc9d60957922f7294
Gary Briceño <gary.briceno@gmail.com>
2026-08-30 16:18:54 -0500

    modify test

--- test/test-package.py
+++ test/test-package.py
@@ -1,6 +1,10 @@
+from pathlib import Path
 import pandas as pd
 from model.predict import make_prediction
 
-sample_input_data = pd.read_csv("~/test/bankchurn_test.csv")
+BASE_DIR = Path(__file__).resolve().parent
+
+sample_input_data = pd.read_csv(BASE_DIR / "bankchurn_test.csv")
+
 result = make_prediction(input_data=sample_input_data)
 print(result)
